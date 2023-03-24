e0c15beaffa366250fa16a5f3ca676fbc7d009af
Narcis <narcis.teodor30@gmail.com>
2023-03-25 00:46:34 +0200

    Add source code.

--- PentaStagiuModule01Week01_Homework/Program.cs
+++ PentaStagiuModule01Week01_Homework/Program.cs
@@ -10,6 +10,51 @@ namespace PentaStagiuModule01Week01_Homework
     {
         static void Main(string[] args)
         {
+            Random GenerateRandom = new Random();
+            int RandomNumber = GenerateRandom.Next(1, 100);
+            int UserGuess;
+
+            do
+            {
+                Console.WriteLine("Guess the number between (1, 100) :)");
+                UserGuess = int.Parse(Console.ReadLine());
+
+                if (UserGuess < 1 || UserGuess > 100)
+                {
+                    Console.WriteLine("You need to choise a number between (1, 100).");
+                    continue;
+                }
+
+                if (UserGuess != RandomNumber)
+                {
+                    if (UserGuess > RandomNumber)
+                        Console.WriteLine("Your guess was higher than the number");
+                    if (UserGuess < RandomNumber)
+                        Console.WriteLine("Your guess was smaller than the number");
+
+                    Console.WriteLine("Do you want to try again? (Y/N)");
+                    string TryAgain = Console.ReadLine();
+                    if (TryAgain.ToUpper() == "Y")
+                    {
+                        continue;
+                    }
+                    else if (TryAgain.ToUpper() == "N")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Undefined answer.");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations!! You won! :)");
+                }
+            } while (UserGuess != RandomNumber);
+
+            Console.WriteLine("The number was {0}", RandomNumber);
         }
     }
 }
